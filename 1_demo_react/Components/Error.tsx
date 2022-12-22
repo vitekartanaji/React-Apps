6a2cf4039a934253109f28e032b90afca2d6953d
vitekartanaji <113625207+vitekartanaji@users.noreply.github.com>
2022-12-22 09:08:24 +0530

    Create Error.tsx

---
+++ 1_demo_react/Components/Error.tsx
@@ -0,0 +1,10 @@
+const ErrorPage=()=>{
+    return(
+        <>
+        <h1 className="Error">404</h1>
+        <h3 className="Error">Not Working</h3>
+        </>
+    )
+}
+
+export default ErrorPage;
